37edf7c4a8b658571a68ebea55ed20e0f2928d2b
Jonathan Frappier <92055993+jonathanfrappier@users.noreply.github.com>
2025-01-21 15:46:16 -0500

    Update outputs.tf

--- vault-dynamic-creds-docker/outputs.tf
+++ vault-dynamic-creds-docker/outputs.tf
@@ -3,17 +3,12 @@ output "POSTGRES_URL" {
   value       = "export TF_VAR_POSTGRES_URL=${docker_container.postgres.network_data.0.ip_address}:5432"
 }
 
-output "POSTGRES_URL_HOST" {
-  description = "Environment variable to access container running PostgreSQL"
-  value       = "export 127.0.0.1:5432"
-}
-
 output "VAULT_ADDR" {
   description = "Environment variable to access container running Vault"
-  value       = "export VAULT_ADDR=http://127.0.0.1:8200" #docker_container.vault.network_data.0.ip_address
+  value       = "export VAULT_ADDR=http://127.0.0.1:8200"
 }
 
 output "VAULT_TOKEN" {
   description = "Environment variable for Vault root token"
-  value       = "export VAULT_TOKEN=root" #docker_container.vault.network_data.0.ip_address
+  value       = "export VAULT_TOKEN=root"
 }
